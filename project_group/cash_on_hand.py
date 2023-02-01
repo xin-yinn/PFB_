d24c58e4d06185777b088568a7c999853844ca8f
Di11on1ee <123861305+Di11on1ee@users.noreply.github.com>
2023-02-01 23:22:57 +0800

    Update cash_on_hand.py

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,3 +1,4 @@
+# import modules
 import csv
 from pathlib import Path
 
@@ -6,17 +7,20 @@ coh_csv = Path.cwd()/'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
 
 def cash_on_hand():
     """
-    function computes the difference in cash-on-hand if current day is lower than previous day
+    function computes the difference in cash-on-hand to see if there are any cash deficits or surplus
     no parameters required
     """
     # check if csv file exists
     if coh_csv.exists():
+        # open file and use mode 'r' to read
         with coh_csv.open(mode = 'r', encoding = 'UTF-8', errors = 'ignore') as csvfile:
             reader = csv.reader(csvfile)
+            # use next to skip header row in csv file
+            next(reader)
+
             #create 2 empty lists the nest data for 'day' and 'cash-on-hand'
             day = []
             coh = []
-            next(reader)
             # create for loop to loop function in every row of the csv file
             for row in reader:
                 # append day and cash on hand into their respective lists
@@ -25,15 +29,17 @@ def cash_on_hand():
     # initialise counter 
     count = 0
     for amount in range(len(coh) - 1):
-        # write formula for difference
+        # difference = coh on current day - coh on next day
         diff = coh[amount] - coh[amount + 1]
-        # if difference more than 0, means there is a cash deficit as current day is more than previous day.
+        # if difference more than 0, means there is a cash deficit as current day is more than next day.
         if diff > 0:
             if file_path.exists():
+                # open file and use mode 'a' to append output into txt file
                 with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
                   file.write(f'\n[CASH DEFICIT] DAY: {day[amount + 1]}, AMOUNT: USD{diff:.0f}')
             count += 1
     #if count remains 0, means that there are no cash deficits in any day, and there is cash surplus
     if count == 0:
+            # open file and use mode 'a' to append output into txt file
             with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
                 file.write(f'\n[CASH SURPLUS] CASH ON HAND ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')                         

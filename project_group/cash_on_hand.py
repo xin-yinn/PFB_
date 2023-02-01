f2a3587cd26b16754e658d4678382831e2091a2e
Di11on1ee <123861305+Di11on1ee@users.noreply.github.com>
2023-02-01 14:46:50 +0800

    Update cash_on_hand.py

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,48 +1,27 @@
-# importing built-in functions from python
-import csv, re
+import csv
 from pathlib import Path
 
-# creating function for cash on hand
+file_path = Path.cwd()/'project_group'/'summary_report.txt'
+coh_csv = Path.cwd()/'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
+
 def cash_on_hand():
-    # reading of csv file extracted from game
-    fp_read = Path.cwd()/"project_group"/"csv_reports"/"cash-on-hand-usd.csv"
-    # creating variable to write to summary text file
-    fp_write = Path.cwd()/"project_group"/"summary_report.txt"
-    # creating empty lists
-    coh_empty_list = []
-    days_empty_list = []
-    amt_empty_list = []
-    diff_empty_list = []
-
-    with fp_read.open(mode= "r", encoding= "UTF-8") as file:
-        coh_reader = csv.reader(file)
-        next(coh_reader)
-        
-        # creating for loop 
-        for line in coh_reader:
-            # appending to empty list
-            coh_empty_list.append(line)
-            # line[0] is the days column
-            day = line[0]
-            # line[1] is the cash on hand amount column
-            amt = line[1]
-            # appending to empty lists
-            days_empty_list.append(int(day))
-            amt_empty_list.append(amt)
-
-            for items in range(len(amt_empty_list)):
-                #create a string to minus the 2 numbers
-                diff = float(amt_empty_list[items]) - float(amt_empty_list[items-1])
-                #put the difference between the numbers into an empty list and append the list
-                diff_empty_list.append(diff)
-
-        with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:    
-            #create for loop to create a zip for 2 appened list creates previousely as category
-            for category in zip(days_empty_list, diff_empty_list):
-                #using if funcion, if the catergory is less than 0,
-                if category[1] <= 0:
-                    #the file will print this
-                    file.write("\n[CASH DEFICIT]" " "f"DAY: {category[0]}, AMOUNT: SGD{category[1]*-1}")
-                else:
-                    #else if the category is more than 0, it will print this
-                    file.write("\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    if coh_csv.exists():
+        with coh_csv.open(mode = 'r', encoding = 'UTF-8', errors = 'ignore') as csvfile:
+            reader = csv.reader(csvfile)
+            day = []
+            coh = []
+            next(reader)
+            for row in reader:
+                day.append(float(row[0]))
+                coh.append(float(row[1]))
+    count = 0
+    for amount in range(len(coh) - 1):
+        diff = coh[amount] - coh[amount + 1]
+        if diff > 0:
+            if file_path.exists():
+                with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
+                  file.write(f'\n[CASH DEFICIT] DAY: {day[amount + 1]}, AMOUNT: USD{diff:.2f}')
+            count += 1
+    if count == 0:
+            with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
+                file.write(f'\n[CASH SURPLUS] CASH ON HAND ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')  
